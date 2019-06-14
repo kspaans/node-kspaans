6ad0f9f80fc3b9c794d8950a561859bbb3437f63
Kyle Spaans <kyle@spaans.ca>
2019-06-13 21:39:03 -0400

    Metadata package about me!

---
+++ README.md
@@ -0,0 +1,14 @@
+# kyle
+
+> npm package with meta data about me!
+
+## Install
+
+```bash
+npm install kyle
+```
+
+---
+> Author: [Kyle Spaans](https://spaans.ca) &bull;
+> Github: [@kspaans](https://github.com/kspaans) &bull;
+> Twitter: [@kspaans](https://twitter.com/kspaans)
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  name: 'Kyle Spaans',
+  label: 'Technologist',
+  email: 'kyle@spaans.ca',
+  phone: '(647) 542-3807',
+  website: 'https://spaans.ca',
+  summary: 'Simplifying the path to production. Code, bass, brew, cycles, and boards.',
+  location: {
+    city: 'Toronto',
+    countryCode: 'CA',
+    region: 'Ontario'
+  },
+  profiles: [
+    { network: 'GitHub',   username: 'kspaans', url: 'https://github.com/kspaans' },
+    { network: 'keybase',  username: 'kspaans', url: 'https://keybase.io/kspaans' },
+    { network: 'LinkedIn', username: 'kspaans', url: 'https://linkedin.com/in/kspaans' },
+    { network: 'npm',      username: 'kspaans', url: 'https://www.npmjs.com/~kspaans' },
+    { network: 'Twitter',  username: 'kspaans', url: 'https://twitter.com/kspaans' }
+  ]
+}
